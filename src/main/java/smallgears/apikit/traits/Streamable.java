c1c22490b51febfa9e9d8e2b6deae48bf7c83a9a
fabiosimeoni <fabio.simeoni@me.com>
2015-02-22 16:12:16 +0100

    basic setup

---
+++ src/main/java/smallgears/apikit/traits/Streamable.java
@@ -0,0 +1,38 @@
+package smallgears.apikit.traits;
+
+import static smallgears.apikit.utils.Apikit.*;
+
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
+
+public interface Streamable<T> extends Iterable<T> {
+
+	/**
+	 * Returns a sequential stream of the value associated with this type.
+	 */
+	default Stream<T> stream() {
+		
+		return streamof(this,false);
+	}
+	
+
+	/**
+	 * Returns a sequential stream of the value associated with this type.
+	 * @param parallel <code>true</code> if the stream is to be consumed in parallel, <code>false</code> otherwise.
+	 */
+	default Stream<T> parallelStream() {
+
+		return streamof(this, true);
+	}
+	
+	/**
+	 * @see Stream#forEach(Consumer)
+	 */
+	default void forEach(Consumer<? super T> consumer) {
+	
+		stream().forEach(consumer);
+	}
+	
+	
+}
---
+++ src/main/java/smallgears/apikit/utils/Apikit.java
@@ -0,0 +1,38 @@
+package smallgears.apikit.utils;
+
+import static java.util.stream.Collectors.*;
+import static java.util.stream.StreamSupport.*;
+
+import java.util.stream.Stream;
+
+import lombok.experimental.UtilityClass;
+
+
+@UtilityClass
+public class Apikit {
+
+	/**
+	 * Turns an {@link Iterable} into a sequential {@link Stream}.
+	 */
+	public static <T> Stream<T> streamof(Iterable<T> vals) {
+		
+		return streamof(vals,false);
+	}
+	
+	/**
+	 * Turns an {@link Iterable} into a {@link Stream}.
+	 */
+	public static <T> Stream<T> streamof(Iterable<T> vals, boolean parallel) {
+		
+		return stream(vals.spliterator(),parallel);
+	}
+	
+	/**
+	 * <code>null</code>-free string joins.
+	 */
+	public static String join(Stream<String> vals) {
+		
+		return vals.filter(s->s!=null).collect(joining());
+	}
+	
+}
